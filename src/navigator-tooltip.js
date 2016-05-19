96502978694ffedb52676010317648aafe125791
Andy Orahoske <andrew.orahoske@zipscene.com>
2016-05-19 10:35:31 -0400

    clean up navigator tooltip src

--- src/navigator-tooltip.js
+++ src/navigator-tooltip.js
@@ -1,28 +1,102 @@
-/* global window */
-
 const { Highcharts: { Scroller, wrap } } = window;
 
 (function() {
+  Scroller.prototype.renderTooltip = function(position, str) {
+    const { chart: { renderer } } = this;
+    this[`${position}Tooltip`] = renderer.rect(0, 0, 0, 0, 3, 2).addClass('fade-out').add(this.navigatorGroup);
+    this[`${position}TooltipText`] = renderer.text(str, 5, 15).addClass('fade-out').add(this.navigatorGroup);
+  };
+
+  const fade = (direction) => {
+    return function(position) {
+      const { [`${position}Tooltip`]: tooltip, [`${position}TooltipText`]: text } = this;
+      const fadeClass = `fade-${direction}`;
+
+      tooltip.attr('class', fadeClass);
+      text.attr('class', fadeClass);
+      this.tooltipArrow.attr('class', fadeClass);
+    };
+  };
+
+  Scroller.prototype.fadeInTooltip = fade('in');
+  Scroller.prototype.fadeOutTooltip = fade('out');
+
+  Scroller.prototype.tooltipPadding = 5;
+  Scroller.prototype.findTooltipPosition = function(position, tooltipWidth) {
+    const { tooltipPadding, scrollerWidth, scrollerLeft, fixedContainerWidth } = this;
+    const handleIndex = { left: 0, right: 1, center: 0 };
+    const handle = this.handles[handleIndex[position]];
+    const offset = ((tooltipWidth + tooltipPadding) / 2);
+    let x = handle.translateX - offset;
+    const arrow = {
+      x: handle.translateX - 5,
+      y: handle.translateY - 15
+    };
+
+    if (position === 'center') {
+      x = handle.translateX + ((fixedContainerWidth - tooltipWidth) / 2);
+      arrow.x = x + (tooltipWidth / 2) - 5;
+    }
+
+    // tooltip is outside right edge of minimap
+    if (x + tooltipWidth > scrollerWidth) {
+      x = scrollerWidth - tooltipWidth;
+      arrow.x = scrollerWidth - 5;
+    }
+
+    // tooltip is outside of left edge of minimap
+    if (x < scrollerLeft) {
+      x = scrollerLeft;
+      arrow.x = x;
+    }
+
+    return { x, y: handle.translateY - 20, arrow };
+  };
+
+  Scroller.prototype.tooltipFill = '#f1eeef';
+  Scroller.prototype.adjustTooltip = function(position, str) {
+    const { tooltipPadding, tooltipFill, [`${position}Tooltip`]: tooltip, [`${position}TooltipText`]: text } = this;
+    const textWidth = text.element.clientWidth;
+    const textHeight = text.element.clientHeight;
+    const { x, y, arrow: { x: arrowX, y: arrowY } } = this.findPosition(position, textWidth);
+    const arrow = this.tooltipArrow;
+
+    tooltip.attr({
+      width: textWidth + tooltipPadding,
+      height: textHeight + tooltipPadding,
+      fill: tooltipFill,
+      x,
+      y: y - textHeight
+    });
+
+    text.attr({ text: str, x: x + (tooltipPadding / 2), y });
+
+    arrow.attr({
+      d: [
+        'M', arrowX, arrowY,
+        'H', arrowX + 10,
+        'L', arrowX + 5, arrowY + 5,
+        'L', arrowX, arrowY
+      ],
+      fill: tooltipFill
+    });
+  };
+
   wrap(Scroller.prototype, 'render', function(proceed, min, max, pxMin, pxMax) {
     const [, ...args] = arguments;
     proceed.call(this, ...args);
 
-    const { chart: { renderer }, navigatorOptions: { tooltipFormatter } } = this;
-    const tooltipPadding = 5;
+    const { chart: { renderer }, navigatorOptions: { tooltipFormatter }, renderTooltip } = this;
 
     if (!tooltipFormatter) return;
     const range = this.xAxis.toFixedRange(this.zoomedMin, this.zoomedMax);
     const formattedTooltipText = tooltipFormatter(range.min, range.max, pxMin, pxMax);
-
-    const renderTooltip = (side, str) => {
-      this[`${side}Tooltip`] = renderer.rect(0, 0, 0, 0, 3, 2).addClass('fade-out').add(this.navigatorGroup);
-      this[`${side}TooltipText`] = renderer.text(str, 5, 15).addClass('fade-out').add(this.navigatorGroup);
-    };
+    formattedTooltipText.center = `${formattedTooltipText.left} - ${formattedTooltipText.right}`;
 
     if (!this.tooltipRendered) {
-      renderTooltip('left', formattedTooltipText.min);
-      renderTooltip('right', formattedTooltipText.max);
-      renderTooltip('center', `${formattedTooltipText.min} - ${formattedTooltipText.max}`);
+      renderTooltip('left', formattedTooltipText.left);
+      renderTooltip('right', formattedTooltipText.right);
+      renderTooltip('center', formattedTooltipText.center);
       this.tooltipArrow = renderer.path([
         'M', 0, 0,
         'H', 10,
@@ -32,108 +106,18 @@ const { Highcharts: { Scroller, wrap } } = window;
       this.tooltipRendered = true;
     }
 
-    if (!formattedTooltipText.min) return;
-
-    const fadeInTooltip = (side) => {
-      const tooltip = this[`${side}Tooltip`];
-      const text = this[`${side}TooltipText`];
-      tooltip.attr('class', 'fade-in');
-      text.attr('class', 'fade-in');
-      this.tooltipArrow.attr('class', 'fade-in');
-    };
-
-    const fadeOutTooltip = function(side) {
-      const tooltip = this[`${side}Tooltip`];
-      const text = this[`${side}TooltipText`];
-      tooltip.attr('class', 'fade-out');
-      text.attr('class', 'fade-out');
-      this.tooltipArrow.attr('class', 'fade-out');
-    }.bind(this);
-
-    const findPosition = function(side, width) {
-      const handleIndex = { left: 0, right: 1, center: 0 };
-      const handle = this.handles[handleIndex[side]];
-      const offset = ((width + tooltipPadding) / 2);
-      let x = handle.translateX - offset;
-      const arrow = {
-        x: handle.translateX - 5,
-        y: handle.translateY - 15
-      };
-
-      if (side === 'center') {
-        x = handle.translateX + ((this.fixedWidth - width) / 2);
-        arrow.x = x + (width / 2) - 5;
+    if (!formattedTooltipText.left) return;
+    const positions = ['left', 'right', 'center'];
+    positions.forEach((position) => {
+      const capitalizedPosition = position.charAt(0).toUpperCase() + position.slice(1);
+      const grabbed = this[`grabbed${capitalizedPosition}`];
+      if (grabbed) {
+        this.fadeInTooltip(position);
+        this.adjustTooltip(position, formattedTooltipText[position]);
+        setTimeout(() => {
+          this.fadeOutTooltip(position);
+        }, 1000);
       }
-
-      if (x + width > this.scrollerWidth) {
-        x = this.scrollerWidth - width;
-        arrow.x = this.scrollerWidth - 5;
-      }
-
-      if (x < this.scrollerLeft) {
-        x = this.scrollerLeft;
-        arrow.x = x;
-      }
-
-      return { x, y: handle.translateY - 20, arrow };
-    }.bind(this);
-
-    const adjustTooltip = function(side, str) {
-      const tooltip = this[`${side}Tooltip`];
-      const text = this[`${side}TooltipText`];
-      const textWidth = text.element.clientWidth;
-      const textHeight = text.element.clientHeight;
-      const pos = findPosition(side, textWidth);
-      const arrow = this.tooltipArrow;
-
-      tooltip.attr({
-        width: textWidth + tooltipPadding,
-        height: textHeight + tooltipPadding,
-        fill: '#f1eeef',
-        x: pos.x,
-        y: pos.y - textHeight
-      });
-
-      text.attr({
-        text: str,
-        x: pos.x + (tooltipPadding / 2),
-        y: pos.y
-      });
-
-      arrow.attr({
-        d: [
-          'M', pos.arrow.x, pos.arrow.y,
-          'H', pos.arrow.x + 10,
-          'L', pos.arrow.x + 5, pos.arrow.y + 5,
-          'L', pos.arrow.x, pos.arrow.y
-        ],
-        fill: '#f1eeef'
-      });
-    }.bind(this);
-
-    if (this.grabbedLeft) {
-      fadeInTooltip('left');
-      adjustTooltip('left', formattedTooltipText.min);
-      setTimeout(function() {
-        fadeOutTooltip('left');
-      }, 1000);
-    }
-
-    if (this.grabbedRight) {
-      fadeInTooltip('right');
-      adjustTooltip('right', formattedTooltipText.max);
-      setTimeout(function() {
-        fadeOutTooltip('right');
-      }, 1000);
-    }
-
-    if (this.grabbedCenter) {
-      fadeInTooltip('center');
-      adjustTooltip('center', `${formattedTooltipText.min} - ${formattedTooltipText.max}`);
-      setTimeout(function() {
-        fadeOutTooltip('center');
-      }, 1000);
-    }
-
+    });
   });
 }());
