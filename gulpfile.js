3ecabf11caaa5157fd9f0f433c0b2fd36cd09e82
Andy Orahoske <andrew.orahoske@zipscene.com>
2016-05-24 10:59:01 -0400

    add in watch task to gulp and set to default

--- gulpfile.js
+++ gulpfile.js
@@ -10,15 +10,29 @@ var buildDir = 'dist';
 
 // Tasks
 //-----------------------------------------------
-gulp.task('build', function () {
-  gulp.src('src/**/*.js')
+gulp.task('build-styles', function() {
+  var stream = gulp.src('src/styles/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(buildDir + '/styles'));
+
+  return stream;
+});
+
+gulp.task('build-scripts', function() {
+  var stream = gulp.src('src/**/*.js')
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(gulp.dest(buildDir));
 
-  gulp.src('src/styles/*.sass')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(buildDir + '/styles'));
-
+  return stream;
 });
+
+gulp.task('build', ['build-scripts', 'build-styles']);
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.js', ['build-scripts']);
+  gulp.watch('src/styles/*.sass', ['build-styles']);
+});
+
+gulp.task('default', ['watch']);
